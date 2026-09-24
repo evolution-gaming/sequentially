1095816f41bf9fda7ca8d8941634290356a87031
Scala Steward <scala_steward@virtuslab.com>
2026-09-24 17:09:08 +0000

    Update akka-to-pekko-adapter-actor, ... to 1.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
   }
 
   object AkkaToPekkoAdapter {
-    private val Version = "1.0.4"
+    private val Version = "1.0.6"
 
     val Actor = "com.evolution" %% "akka-to-pekko-adapter-actor" % Version
     val Stream = "com.evolution" %% "akka-to-pekko-adapter-stream" % Version
